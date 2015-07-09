77ef088aa695fa1fc716fd548ccbe02cdb14bdb2
Pasindu De Silva <ppasindud@gmail.com>
2015-07-09 16:02:20 +0800

    Added scripts to convert google csv form to yaml

---
+++ scripts/data/2015-07-09-Sinhala-Transliteration-(සිංහල).yaml
@@ -0,0 +1,10 @@
+layout: default
+title: Sinhala Transliteration (සිංහල)
+link: http://apps.microsoft.com/windows/en-us/app/sinhala-transliteration/63d4e2d6-0661-4e16-be56-7183953845d4
+desc:"The easiest way to type in Sinhala on windows 8.1
+Get the Sinhala typing app Free from Windows Store.."
+categories:
+- sinhalatyping
+tags:
+- app
+---
---
+++ scripts/script.py
@@ -0,0 +1,17 @@
+import csv
+import os
+
+def main():
+
+    with open('SinhalaWebesponses1.csv', 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in spamreader:
+            print row[1]
+
+            with open("data/2015-07-09-"+row[1].replace(" ","-")+".yaml", "w") as myfile:
+                myfile.write("layout: default\ntitle: "+row[1]+"\nlink: "+row[2]+"\ndesc:\""+row[7]+"\"\ncategories:\n- "+row[6]+"\ntags:\n- "+row[8]+"\n---")
+
+if __name__ == '__main__':
+    main()
+
+
